d17bfa928f8243541cc7b8c38d86678b07333154
Inom T <i@inomoz.ru>
2022-08-14 14:52:44 +0300

    remove grip

--- .doom.d/style.el
+++ .doom.d/style.el
@@ -44,7 +44,7 @@
   ;; Custom modeline items order
   (doom-modeline-def-modeline 'main
     '(persp-name bar workspace-name window-number modals matches buffer-info remote-host buffer-position word-count parrot selection-info)
-    '(objed-state misc-info battery grip irc mu4e gnus github debug lsp minor-modes input-method indent-info buffer-encoding major-mode process vcs checker))
+    '(objed-state misc-info battery irc mu4e gnus github debug lsp minor-modes input-method indent-info buffer-encoding major-mode process vcs checker))
 
   ;; Show workspace name
   (setq doom-modeline-persp-name t)
