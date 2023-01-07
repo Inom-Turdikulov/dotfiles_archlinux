baeaea8fbd4e6310f9a9f358dc704350ec7280f2
Inom T <i@inomoz.ru>
2023-01-07 23:08:33 +0600

    Add useful binaries/functions, modified existing

---
+++ .local/bin/pomo.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if [[ "$1" == "--minutes="* ]]; then
+    MINUTES="${1#--minutes=}"; shift
+elif [[ "$1" == "-m"* ]]; then
+    MINUTES="${1#-m}"; shift
+    if [ "$MINUTES" == "" ]; then
+        MINUTES="${1}"; shift
+    fi
+fi
+
+MINUTES=${MINUTES:-25}
+
+trap "timew cancel && exit" INT TERM
+
+notify-send -u critical "Pomodoro Start!"
+timew start pomodoro $@ 2>&1 1>/dev/null
+sleep $(( $MINUTES * 60 ))
+
+
+timew stop 2>&1 1>/dev/null
+notify-send -u critical "Pomodoro Finished!"
