e4a18a6742c465b579570249df8c986c20d9bbf7
Inom T <i@inomoz.ru>
2023-01-20 01:01:59 +0600

    Get wallpaper recursevely

--- .local/bin/random-wallpaper.sh
+++ .local/bin/random-wallpaper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [ -d "$HOME/Pictures/wallpapers/" ]; then
-   wfile=$(find ~/Pictures/wallpapers/*.jpg -type f | shuf -n 1)
+   wfile=$(find ~/Pictures/wallpapers/ -type f | shuf -n 1)
    ext="${wfile##*.}"
    if [ "$ext" == jpg ]; then
       feh --bg-fill $wfile &
